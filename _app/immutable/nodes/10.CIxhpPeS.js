7be1617a07ea1583436795970f14d7b53a032ec6
Bruno Thomas <bruno@barreverte.fr>
2024-04-12 17:18:05 +0200

    Updates

---
+++ _app/immutable/nodes/10.CIxhpPeS.js
@@ -0,0 +1,24 @@
+import{s as D,a as f,e as v,t as H,l as L,f as c,g as b,c as h,b as x,d as P,o as N,m as $,i as S,h as o,j as I,n as T,k as M}from"../chunks/scheduler.BVJuVc66.js";import{S as O,i as R}from"../chunks/index.B_riHpyU.js";import{$ as k}from"../chunks/runtime.DYiWd5Kn.js";function w(d){let n,a,s,e,m,p=d[0]("header.values")+"",g,q,i,j=`Fermement convaincus que l’urgence écologique et sociale doit nous mener à transformer nos modèles économiques
+			et sociétaux, nous sommes guidés par des valeurs fondamentales qui se traduisent également par des partis pris
+			dans l’accompagnement que nous proposons :`,_,u,C=`<li>Responsabilité environnementale : nos prestations visent avant tout à accompagner nos clients dans la
+				transition
+				écologique. Nous nous appliquons également la notion de responsabilité environnementale en tentant de minimiser
+				l’empreinte écologique de nos activités.</li> <li>Responsabilité sociétale : les enjeux sociaux sont indissociables de la transition écologique. Dans le secteur
+				du numérique, ces enjeux trouvent un écho à travers notamment les notions d’accessibilité, de respect du temps
+				d’attention des utilisateurs, de protection des données personnelles…</li> <li>Sobriété : nous considérons que la sobriété est la conséquence logique de la nécessaire transition
+				écologique.
+				Nous ne croyons pas à un techno-solutionnisme miraculeux qui nous permettrait de maintenir le mode de vie
+				occidental actuel tout en respectant les limites planétaires : nous sommes convaincus – car la science est
+				unanime
+				à ce sujet – que la sobriété (heureuse!) est la voie à suivre et à promouvoir pour un futur durable. Pas de
+				transition écologique sans sobriété : là où c&#39;est possible, nous proposerons toujours des solutions orientées
+				vers
+				moins d&#39;utilisation des ressources, vers des changements de comportements, vers de la low-tech.</li> <li>Open source et communs : nous croyons en une économie des communs. Nous privilégions systématiquement le
+				recours
+				à des outils open source et nous contribuons également à plusieurs projets de communs, numériques ou pas. Dès
+				que
+				c&#39;est possible, nous travaillons en open data et partageons les enseignements tirés de nos accompagnements.</li> <li>Transparence et pédagogie : nous tentons d&#39;être aussi transparents que possible sur la manière dont nos
+				services
+				et notre organisation fonctionnent. Nous tentons de vous emmener aussi souvent que possible sous le capot de nos
+				outils, et essayons de vulgariser les sujets les plus techniques afin que chacun puisse comprendre les enjeux
+				d&#39;aujourd&#39;hui.</li>`;return document.title=n=d[0]("header.values"),{c(){a=f(),s=v("div"),e=v("section"),m=v("h1"),g=H(p),q=f(),i=v("p"),i.textContent=j,_=f(),u=v("ul"),u.innerHTML=C,this.h()},l(t){L("svelte-1daps3p",document.head).forEach(c),a=b(t),s=h(t,"DIV",{class:!0});var y=x(s);e=h(y,"SECTION",{class:!0});var r=x(e);m=h(r,"H1",{});var E=x(m);g=P(E,p),E.forEach(c),q=b(r),i=h(r,"P",{"data-svelte-h":!0}),N(i)!=="svelte-13hnx02"&&(i.textContent=j),_=b(r),u=h(r,"UL",{"data-svelte-h":!0}),N(u)!=="svelte-811222"&&(u.innerHTML=C),r.forEach(c),y.forEach(c),this.h()},h(){$(e,"class","values__content"),$(s,"class","values")},m(t,l){S(t,a,l),S(t,s,l),o(s,e),o(e,m),o(m,g),o(e,q),o(e,i),o(e,_),o(e,u)},p(t,[l]){l&1&&n!==(n=t[0]("header.values"))&&(document.title=n),l&1&&p!==(p=t[0]("header.values")+"")&&I(g,p)},i:T,o:T,d(t){t&&(c(a),c(s))}}}function F(d,n,a){let s;return M(d,k,e=>a(0,s=e)),[s]}class A extends O{constructor(n){super(),R(this,n,F,w,D,{})}}export{A as component};
