4494aa6aa9a67a5a579f040bf743f79f68f05c61
Marc Bouvier <m.bouvier.dev@gmail.com>
2024-11-07 19:29:37 +0100

    Updates

--- _app/immutable/entry/start.xJjoxhWY.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.ClR--HZ8.js";export{t as start};
---
+++ _app/immutable/nodes/3.C92FLhi0.js
@@ -0,0 +1,29 @@
+import{s as E,h as w,a as z,t as C,b as D,c as i}from"../chunks/disclose-version.DJYIB9VQ.js";import{i as P}from"../chunks/legacy.Ji9MqoNY.js";import{p as T,f as A,t as n,a as I,$ as L,s as t,c as e,r as s,n as _}from"../chunks/utils.DhO5QomL.js";import{$ as O}from"../chunks/runtime.tWU7zkft.js";var Q=C(`<h2> </h2> <div class="about"><section class="about__values"><h1> </h1> <p>Fermement convaincus que l’urgence écologique et sociale doit nous mener à transformer nos modèles économiques
+			et sociétaux, nous sommes guidés par des valeurs fondamentales qui se traduisent également par des partis pris
+			dans l’accompagnement que nous proposons :</p> <ul><li>Responsabilité environnementale : nos prestations visent avant tout à accompagner nos clients dans la
+				transition
+				écologique. Nous nous appliquons également la notion de responsabilité environnementale en tentant de minimiser
+				l’empreinte écologique de nos activités.</li> <li>Responsabilité sociétale : les enjeux sociaux sont indissociables de la transition écologique. Dans le secteur
+				du numérique, ces enjeux trouvent un écho à travers notamment les notions d’accessibilité, de respect du temps
+				d’attention des utilisateurs, de protection des données personnelles…</li> <li>Sobriété : nous considérons que la sobriété est la conséquence logique de la nécessaire transition
+				écologique.
+				Nous ne croyons pas à un techno-solutionnisme miraculeux qui nous permettrait de maintenir le mode de vie
+				occidental actuel tout en respectant les limites planétaires : nous sommes convaincus – car la science est
+				unanime
+				à ce sujet – que la sobriété (heureuse!) est la voie à suivre et à promouvoir pour un futur durable. Pas de
+				transition écologique sans sobriété : là où c'est possible, nous proposerons toujours des solutions orientées
+				vers
+				moins d'utilisation des ressources, vers des changements de comportements, vers de la low-tech.</li> <li>Open source et communs : nous croyons en une économie des communs. Nous privilégions systématiquement le
+				recours
+				à des outils open source et nous contribuons également à plusieurs projets de communs, numériques ou pas. Dès
+				que
+				c'est possible, nous travaillons en open data et partageons les enseignements tirés de nos accompagnements.</li> <li>Transparence et pédagogie : nous tentons d'être aussi transparents que possible sur la manière dont nos
+				services
+				et notre organisation fonctionnent. Nous tentons de vous emmener aussi souvent que possible sous le capot de nos
+				outils, et essayons de vulgariser les sujets les plus techniques afin que chacun puisse comprendre les enjeux
+				d'aujourd'hui.</li></ul></section> <section class="about__project"><h1> </h1> <p>Rezofora a pour ambition d’accompagner les organisations dans la transition écologique et sociale grâce à des
+			prestations de conseil, de formation et de sensibilisation. Spécialisé dans le domaine du numérique responsable,
+			Rezofora offre des services articulés selon trois axes :</p> <ul><li>Conseil et accompagnement : mise en place de démarches RSE et numérique responsable, reporting de durabilité,
+				mesure d’impact, éco-conception numérique…</li> <li>Formation et sensibilisation : Fresques du Climat, du Numérique et des Low-Tech, formations sur mesure en
+				RSE/numérique responsable</li> <li>Inspiration et transformation des modèles : learning expeditions, ateliers fiction/prospective et ateliers sur
+				mesure pour imaginer le numérique et la tech autrement.</li></ul></section> <section class="about__ecosystem"><h1>Partenaires/écosystème</h1> <p> </p></section> <section class="about__team"><h1>Equipe</h1> <p> </p></section> <section class="about__faq"><h1>FAQ</h1> <p> </p></section></div>`,1);function K(f,x){T(x,!1);const j=E(),o=()=>D(O,"$_",j);P();var m=Q();w(k=>{n(()=>L.title=o()("header.about"))});var a=A(m),$=e(a,!0);n(()=>i($,o()("header.training"))),s(a);var p=t(a,2),r=e(p),d=e(r),y=e(d,!0);n(()=>i(y,o()("header.values"))),s(d),_(4),s(r);var u=t(r,2),v=e(u),R=e(v,!0);n(()=>i(R,o()("header.project"))),s(v),_(4),s(u);var l=t(u,2),g=t(e(l),2),N=e(g,!0);n(()=>i(N,o()("temp.lipsum"))),s(g),s(l);var c=t(l,2),h=t(e(c),2),F=e(h,!0);n(()=>i(F,o()("temp.lipsum"))),s(h),s(c);var q=t(c,2),b=t(e(q),2),S=e(b,!0);n(()=>i(S,o()("temp.lipsum"))),s(b),s(q),s(p),z(f,m),I()}export{K as component};
