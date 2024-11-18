84a72f6633643c909049b8809f962120d8261a3b
Marc Bouvier <m.bouvier.dev@gmail.com>
2024-11-18 11:31:38 +0100

    fix: lint errors

--- src/lib/SchemaOrg.svelte
+++ src/lib/SchemaOrg.svelte
@@ -1,63 +1,64 @@
 <script lang="ts">
 	import * as m from '$lib/paraglide/messages.js';
+
+	/* eslint-disable svelte/no-at-html-tags */
+
 	let schemaOrg = {
-		"@context": "https://schema.org",
-		"@graph": [
+		'@context': 'https://schema.org',
+		'@graph': [
 			{
-				"@type": "WebPage",
-				"@id": "https://rezofora.co/",
-				"url": "https://rezofora.co/",
-				"name": m.head_seo_webPage_name(),
-				"about": {
-					"@id": "https://rezofora.co/"
+				'@type': 'WebPage',
+				'@id': 'https://rezofora.co/',
+				url: 'https://rezofora.co/',
+				name: m.head_seo_webPage_name(),
+				about: {
+					'@id': 'https://rezofora.co/'
 				},
-				"datePublished": "2024-03-08T18:00:00+00:00",
-				"dateModified": "2024-03-08T18:00:00+00:00",
-				"description":m.head_seo_webPage_description(),
-				"inLanguage": m.head_seo_webPage_inLanguage(),
-				"potentialAction": [
+				datePublished: '2024-03-08T18:00:00+00:00',
+				dateModified: '2024-03-08T18:00:00+00:00',
+				description: m.head_seo_webPage_description(),
+				inLanguage: m.head_seo_webPage_inLanguage(),
+				potentialAction: [
 					{
-						"@type": "ReadAction",
-						"target": ["https://rezofora.co/"]
+						'@type': 'ReadAction',
+						target: ['https://rezofora.co/']
 					}
 				]
 			},
 			{
-				"@type": "WebSite",
-				"@id": "https://rezofora.co/",
-				"url": "https://rezofora.co/",
-				"name": m.head_seo_webSite_name(),
-				"description": m.head_seo_webSite_name(),
-				"publisher": {
-					"@id": "https://rezofora.co/"
+				'@type': 'WebSite',
+				'@id': 'https://rezofora.co/',
+				url: 'https://rezofora.co/',
+				name: m.head_seo_webSite_name(),
+				description: m.head_seo_webSite_name(),
+				publisher: {
+					'@id': 'https://rezofora.co/'
 				},
-				"inLanguage": m.head_seo_webSite_inLanguage()
+				inLanguage: m.head_seo_webSite_inLanguage()
 			},
 			{
-				"@type": ["Person", "Organization"],
-				"@id": "https://rezofora.co/",
-				"name": m.head_seo_personOrganization_name(),
-				"sameAs": [
-					"https://rezofora.co/",
-					"https://rezofora.fr/",
-					"https://rezofora.io/",
-					"https://rezofora.co/",
-					"https://www.linkedin.com/in/adelineagut/"
+				'@type': ['Person', 'Organization'],
+				'@id': 'https://rezofora.co/',
+				name: m.head_seo_personOrganization_name(),
+				sameAs: [
+					'https://rezofora.co/',
+					'https://rezofora.fr/',
+					'https://rezofora.io/',
+					'https://rezofora.co/',
+					'https://www.linkedin.com/in/adelineagut/'
 				]
 			}
 		]
-	}
+	};
 
-	const jsonSchemaOrg = JSON.stringify(schemaOrg, null, 2)
+	const jsonSchemaOrg = JSON.stringify(schemaOrg, null, 2);
 	const schemaOrgMarkup = `
-${'<'}script type=\"application/ld+json\">
-   ${jsonSchemaOrg	}
-${'<'}/script>`
-
+${'<'}script type="application/ld+json">
+   ${jsonSchemaOrg}
+${'<'}/script>`;
 </script>
 
 <svelte:head>
-
 	{@html schemaOrgMarkup}
 
 	<meta
