fe7526b8b1de3265087dd5e5f928baa84f9aad4a
Marc Bouvier <m.bouvier.dev@gmail.com>
2024-11-18 18:56:17 +0100

    i18n: Legal page translation strings

--- src/routes/legal/+page.svelte
+++ src/routes/legal/+page.svelte
@@ -20,30 +20,31 @@
 	<div class="wrapper title-and-text">
 
 		<header>
-			<h2> Éditeur</h2>
+			<h2>{m.legal_editor_title()}</h2>
 		</header>
+
 
 		<div class="center-in-section">
 
-			<h3>Raison sociale</h3>
+			<h3>{m.legal_editor_business_name_title()}</h3>
 			<p>IROCO SAS</p>
 
-			<h3>Adresse du siège social</h3>
+			<h3>{m.legal_editor_business_address_title()}</h3>
 			<address>
 				3 rue Aristide Briand<br />
 				92170 VANVES
 			</address>
 
-			<h3>Contact</h3>
+			<h3>{m.legal_editor_business_contact_title()}</h3>
 			<p><a href="mailto:hello@iroco.co">hello@iroco.co</a></p>
 
-			<h3>Montant du capital social</h3>
+			<h3>{m.legal_editor_business_funding_amount_title()}</h3>
 			<p>2 000 euros</p>
 
-			<h3>SIRET</h3>
+			<h3>{m.legal_editor_business_siret_title()}</h3>
 			<p>88942425500012</p>
 
-			<h3>Nom du directeur et/ou co-directeur</h3>
+			<h3>{m.legal_editor_business_chair_executives_names_title()}</h3>
 			<p>Adeline AGUT, Bruno THOMAS</p>
 
 		</div>
@@ -55,23 +56,24 @@
 	<div class="wrapper title-and-text">
 
 		<header>
-			<h2>Hébergeur</h2>
+			<h2>{m.legal_hosting_title()}</h2>
 		</header>
 
 		<div class="center-in-section">
 
-			<h3>Raison Sociale</h3>
+			<h3>{m.legal_hosting_name_title()}</h3>
 			<p>Eolas SARL</p>
 
-			<h3>Adresse du siège social</h3>
-			<address>29 rue Servan<br />
+			<h3>{m.legal_hosting_address_title()}</h3>
+			<address>
+				29 rue Servan<br />
 				38000 GRENOBLE
 			</address>
 
-			<h3>Téléphone</h3>
+			<h3>{m.legal_hosting_phone_title()}</h3>
 			<p><a href="tel:+33476445050">+33 4 76 44 50 50</a></p>
 
-			<h3>Montant du Capital Social</h3>
+			<h3>{m.legal_hosting_funding_amount_title()}</h3>
 			<p>28 988 euros</p>
 
 		</div>
